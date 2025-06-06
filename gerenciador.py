645b7b89f6c327903a2e2b1f3261494adfb8105e
Rafael Cleber <rafaelcleber@hotmail.com>
2025-06-06 12:40:33 -0300

    Criando interface

--- gerenciador.py
+++ gerenciador.py
@@ -1,9 +1,16 @@
-import tinker as tk
+import tkinter as tk
 
 
 
 
 janela = tk.Tk()
+
+janela.title('Gerenciador de Despesa')
+
+tk.Label(janela, text='Valor (R$): ').grid(row=0, column=0, sticky='e')
+valor_input = tk.Entry(janela)
+valor_input.grid(row=0, column=1, sticky='w')
+
 
 
 
